2164fe12e220fbee2cc8658f26f7e96a7336d27b
Saqib Sattar <159882823+saqibsattar07@users.noreply.github.com>
2024-10-12 19:37:33 +0500

    personal-portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Hero from "../../components/Hero";
+import { FloatingNav } from "../../components/ui/FloatingNav";
+import Grid from "../../components/Grid";
+import RecentProjects from "../../components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "../../components/Clients";
+import Experience from "../../components/Experience";
+import Approach from "../../components/Approach";
+import Footer from "../../components/Footer";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        {/* <Clients /> */}
+        {/* <Experience /> */}
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
